2cd1ad34db571231ce5f1150fd5eddbec39192ed
“[Gyanaram <[gyanaramnitian@gmail.com]>
2024-12-17 17:11:24 +0530

    hello

--- Socket/Socket.js
+++ Socket/Socket.js
@@ -9,7 +9,7 @@ const server= http.createServer(app)
 const io= new Server(server,
     {
         cors:{
-            origin: ["https://instaclone-1-187b.onrender.com", "http://localhost:5173"],
+            origin: ["https://message-app-virid.vercel.app/", "http://localhost:5173"],
             methods:['GET','POST']
         }
     }
