8aa306785196e5df05b5d86f92e28704aef46f8a
“[Gyanaram <[gyanaramnitian@gmail.com]>
2024-12-17 22:00:02 +0530

    hello

--- Socket/Socket.js
+++ Socket/Socket.js
@@ -7,9 +7,9 @@ const app= express();
 const server= http.createServer(app)
 
 const io= new Server(server,
-    {                               //"https://message-app-virid.vercel.app/",
+    {                               //",
         cors:{
-            origin:  "http://localhost:5173",
+            origin: ["http://localhost:5173","https://message-app-virid.vercel.app"],
             methods:['GET','POST']
         }
     }
