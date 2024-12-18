a3370beba6b08691ebd4a2e952fc625337f2f9cf
“[Gyanaram <[gyanaramnitian@gmail.com]>
2024-12-18 16:28:13 +0530

    helo

--- Socket/Socket.js
+++ Socket/Socket.js
@@ -15,25 +15,30 @@ const io= new Server(server,
     }
 )
 const usersocketmap={}
-const GetReciverSocketId = (receiverId) => {
-    console.log("Checking receiverId:", receiverId);
-    console.log("Current usersocketmap:", usersocketmap);
-    return usersocketmap[receiverId];
-};
+
 io.on('connection',(socket)=>{
     const userid= socket.handshake.query.userid;
+    
     if(userid){
         console.log(`user connected userid= ${userid} socketid=${socket.id}`)
         usersocketmap[userid] = socket.id;
+        console.log('Updated usersocketmap:', usersocketmap);
+        console.log("fcfc",usersocketmap[userid])
     }
     io.emit('getonlineusers',Object.keys(usersocketmap))
     socket.on('disconnect',()=>{
         if(userid){
              console.log(`user disconnected userid= ${userid} socketid=${socket.id}`)
+           
              delete usersocketmap[userid];
+             console.log('Updated usersocketmap2:', usersocketmap);
         }
         io.emit('getonlineusers',Object.keys(usersocketmap))
     })
 })
-
+const GetReciverSocketId = (receiverId) => {
+    console.log("Checking receiverId:", receiverId);
+    console.log("Current usersocketmap:", usersocketmap);
+    return usersocketmap?.[receiverId];
+};
 module.exports={server,io,app,GetReciverSocketId};
